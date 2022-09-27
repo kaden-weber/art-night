12fbd89ef8490a4c89d816e719c344881272b202
Kaden Weber <kaden.weber@showpad.com>
2022-09-27 19:28:38 -0400

    use curves with different height and frequency

--- src/Artist.ts
+++ src/Artist.ts
@@ -1,94 +1,111 @@
 // radians = (Math.PI / 180) * degrees;
 
 // Calculates y position from x
-function calcSineY(x, h, yOffset, f, w) {
+function calcSineY(x: number, h: number, f: number, canvasWidth: number, canvasHeight: number) {
     // h is the amplitude of the wave
     // x is the current x value we get every time interval
     // 2 * PI is the length of one cycle (full circumference)
     // f/w is the frequency fraction
-    return yOffset + h - h * Math.sin(x * 2 * Math.PI * (f / w))
+    return (canvasHeight / 2) - h * Math.sin(x * 2 * Math.PI * (f / canvasWidth))
 }
 
-// function randInt(min, max) {
-//     return Math.floor(Math.random() * (max - min)) + min
-// }
+function randInt(min: number, max: number): number {
+    return Math.floor(Math.random() * (max - min)) + min
+}
+
+class Curve {
+    public amplitude: number
+    public frequency: number // cycles per length of canvas
+
+    constructor(frequency: number, amplitude: number) {
+        this.frequency = frequency
+        this.amplitude = amplitude
+    }
+}
 
 class Artist {
     private canvas: HTMLCanvasElement
     private context: CanvasRenderingContext2D
 
+    // an appropriate maximum height for waves
+    private drawHeight: number
+    // state 
     private x: number
-    private frequency: number // How many cycles per canvas width => Frequency: Tone & Speed
     private erasing: Boolean
+    private lineCount: number
+    private currentCurve: Curve
 
     constructor() {
-
-        let canvas = document.getElementById("sines") as HTMLCanvasElement
-        let context = canvas.getContext("2d")
-        canvas.width = window.innerWidth
-        canvas.height = window.innerHeight
-        this.canvas = canvas
+        this.canvas = document.getElementById("sines") as HTMLCanvasElement
+        let context = this.canvas.getContext("2d")
         this.context = context
-
         this.start()
     }
 
     private drawSine(x: number) {
-        // erasing everything again
-        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height)
         // draw sin curve point to point until x
         this.context.beginPath() // Draw a new path
         this.context.strokeStyle = "black" // Pick a color
         for (var i = 0; i < x; i++) {
             // Loop from left side to current x
-            var y = calcSineY(i, this.canvas.height / 4, this.canvas.height / 4, this.frequency, this.canvas.width) // Calculate y value from x
+            var y = calcSineY(i, this.currentCurve.amplitude, this.currentCurve.frequency, this.canvas.width, this.canvas.height) // Calculate y value from x
             this.context.lineTo(i, y) // Where to draw to
         }
         this.context.stroke() // Draw
     }
 
-    private erase(x) {
+    private erase(x: number) {
         this.context.clearRect(x, 0, 4, this.canvas.height)
     }
 
-    private animation() {
+    private checkWindowSize() {
+        this.canvas.width = window.innerWidth
+        this.canvas.height = window.innerHeight
+    }
 
+    private determineNextBehavior() {
+        if (this.erasing) {
+            this.erasing = false
+        } else {
+            this.lineCount++
+            this.currentCurve.amplitude = randInt(this.drawHeight / 2, this.drawHeight)
+            this.currentCurve.frequency = randInt(10, 90) / 10
+        }
+
+        if (this.lineCount == 2) {
+            this.erasing = true
+            this.lineCount = 0
+        }
+    }
+
+    private animation() {
         if (this.erasing) {
             this.erase(this.x)
         } else {
-
             this.drawSine(this.x) // Call draw function every cycle
         }
+
         this.x++ // Increment x by 1
         if (this.x > this.canvas.width) {
-            this.erasing = !this.erasing
-            this.x = 0 // x cannot be more than canvas width, so back to 0
-            this.frequency++ // increment frequency for demonstration
-        }
-        if (this.frequency > 10) {
-            this.frequency = 1
+            this.determineNextBehavior()
+            this.x = 0 // reset drawing cursor
         }
     }
 
     private start() {
-        this.context.fillRect(0, 0, 30, 30)
-        console.log(this.canvas.width)
+        this.checkWindowSize()
+
         // Define initial value of x position (leftmost side of canvas)
         this.x = 0
-        this.frequency = 1
         this.erasing = false
-        // Start time interval
+        this.lineCount = 0
+        this.drawHeight = this.canvas.height / 4
 
-        setInterval(() => this.animation(), 5) // Loop every 5 milliseconds
+        this.currentCurve = new Curve(1, this.drawHeight)
+
+        // Start time interval
+        setInterval(() => this.animation(), 1) // Loop every 5 milliseconds
     }
 }
 
 new Artist()
-
-// var canvas = document.getElementById("sines") // Grab canvas object
-// if (canvas !== null) {
-//     canvas.width = window.innerWidth
-//     canvas.height = window.innerHeight
-//     var ctx = canvas.getContext("2d") // Define canvas context
-//     var w = canvas.width // Canvas width => Frequency is relative to this
-//     var h = canvas.height / 2 // Canvas height over two => Amplitude: Volume
