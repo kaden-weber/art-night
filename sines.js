1a02af2798de26188ae37516a3ca2693a550f0b6
Kaden Weber <kaden.weber@showpad.com>
2022-09-26 21:15:38 -0400

    erase from left to right

--- sines.js
+++ sines.js
@@ -1,11 +1,11 @@
 // radians = (Math.PI / 180) * degrees;
 
-var c = document.getElementById("sines"); // Grab canvas object
-c.width = window.innerWidth;
-c.height = window.innerHeight;
-var ctx = c.getContext("2d"); // Define canvas context
-var w = c.width; // Canvas width => Frequency is relative to this
-var h = c.height / 2; // Canvas height over two => Amplitude: Volume
+var canvas = document.getElementById("sines"); // Grab canvas object
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+var ctx = canvas.getContext("2d"); // Define canvas context
+var w = canvas.width; // Canvas width => Frequency is relative to this
+var h = canvas.height / 2; // Canvas height over two => Amplitude: Volume
 var f = 1; // How many cycles per canvas width => Frequency: Tone & Speed
 
 // Calculates y position from x
@@ -18,7 +18,7 @@ function calcSineY(x) {
 }
 
 function drawSine(x) {
-    ctx.clearRect(0, 0, w, h * 2);
+    ctx.clearRect(0, 0, w, canvas.height);
     // draw sin curve point to point until x
     ctx.beginPath(); // Draw a new path
     ctx.strokeStyle = "black"; // Pick a color
@@ -30,17 +30,27 @@ function drawSine(x) {
     ctx.stroke(); // Draw
 }
 
+function erase(x) {
+    ctx.clearRect(x, 0, 4, canvas.height);
+}
+
 // Define initial value of x positiom (leftmost side of canvas)
 var x = 0;
+var erasing = false;
 // Start time interval
 var interval = setInterval(function () {
-    drawSine(x); // Call draw function every cycle
+    if (erasing) {
+        erase(x);
+    } else {
+        drawSine(x); // Call draw function every cycle
+    }
     x++; // Increment x by 1
     if (x > w) {
+        erasing = !erasing;
         x = 0; // x cannot be more than canvas width, so back to 0
         f++; // increment frequency for demonstration
     }
     if (f > 10) {
         f = 1;
     }
-}, 10); // Loop every 10 milliseconds
+}, 5); // Loop every 5 milliseconds
